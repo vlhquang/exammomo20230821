1778a19bf0592f530ff11daf598354c900e16cb9
vlhquang <75291430+vlhquang@users.noreply.github.com>
2023-08-21 23:15:17 +0700

    Cập nhật thư viện JUnit Test

--- src/test/java/exammomo20230821/com/quangvlh/action/impl/CreateBillActionTest.java
+++ src/test/java/exammomo20230821/com/quangvlh/action/impl/CreateBillActionTest.java
@@ -1,7 +1,7 @@
 package exammomo20230821.com.quangvlh.action.impl;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
